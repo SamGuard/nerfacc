4f2489e5cf4bb31c1ccdc1d0713ed2bbd8158644
Sam Guard <sg2268@bath.ac.uk>
2022-10-20 15:18:30 +0100

    added numpy test

--- test_torch.py
+++ test_torch.py
@@ -1,5 +1,12 @@
+from curses import echo
 import torch
 
 print(torch.__version__)
 print(torch.cuda.is_available())
 print(torch.version.cuda)
+
+try:
+  import numpy
+  torch.zeros(1).cuda()
+except:
+  print("No numpy installed")
